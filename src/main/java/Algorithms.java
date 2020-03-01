82f5f52a92fde7b9d7b0be49a602103e6e85a1ae
Roger <rogerpietrzak92@gmail.com>
2020-03-01 14:27:32 +0100

    ...

--- src/main/java/Algorithms.java
+++ src/main/java/Algorithms.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalLong;
+
 /**
  * ...comment class...
  *
@@ -5,5 +10,11 @@
  * @since 01.03.2020
  */
 final class Algorithms {
+    public static int NWD(int first, int second) {
+        return 0;
 
+    }
+    public static int abs(int a){
+        return 0;
+    }
 }
--- src/test/java/AlgorithmsTest.java
+++ src/test/java/AlgorithmsTest.java
@@ -1,4 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * ...comment class...
@@ -7,5 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 01.03.2020
  */
 class AlgorithmsTest {
-
+    @Test
+    void testNWD(){
+        assertEquals(5,Algorithms.NWD(5,5));
+        assertEquals(8,Algorithms.NWD(16,8));
+        assertEquals(9,Algorithms.NWD(81,18));
+        assertEquals(1,Algorithms.NWD(13,3));
+    }
+    @Test
+    void testAbs(){
+        assertEquals(5,Algorithms.abs(5));
+        assertEquals(6,Algorithms.abs(-6));
+        assertEquals(0,Algorithms.abs(0));
+        assertEquals(50,Algorithms.abs(-50));
+    }
 }
